59b4012b340f7505f906c851b7abdad0a0458dc3
Eefenton <ethanfenton9@gmail.com>
2021-02-24 23:00:49 +0000

    fixing circularity issue

---
+++ walker/index.js
@@ -0,0 +1,84 @@
+/* global $, sessionStorage */
+
+$(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
+  
+function runProgram(){
+  ////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////// SETUP /////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+
+  // Constant Variables
+  var FRAME_RATE = 60;
+  var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+
+  var KEY = {
+  "ENTER": 13,
+  "ArrowDown": 40,
+  "ArrowUp": 38,
+  "ArrowLeft": 37,
+  "ArrowRight": 39, 
+}
+  // Game Item Objects
+var positionX = 0;
+var speedX = 0;
+var positionY = 0;
+var speedY = 0; 
+
+  // one-time setup
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////// CORE LOGIC ///////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+
+  /* 
+  On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
+  by calling this function and executing the code inside.
+  */
+  function newFrame() {
+    
+
+  }
+  
+  /* 
+  Called in response to events.
+  */
+  function handleKeyDown(event) {
+       if (event.which === KEY.ENTER) {
+    console.log("ENTER pressed");
+       }
+       if (event.which === KEY.ArrowDown) {
+    console.log("ArrowDown pressed");
+       }
+       if (event.which === KEY.ArrowLeft) {
+    console.log("ArrowLeft pressed");
+       }
+       if (event.which === KEY.ArrowRight) {
+    console.log("ArrowRight pressed");
+       }
+       if (event.which === KEY.ArrowUp) {
+    console.log("ArrowRight pressed");
+       }
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+ function repositionGameItem() {
+     positionX += speedX;
+    }
+
+ function redrawGameItem() {
+     $("#box").css("left", positionX);
+    }
+
+  function endGame() {
+    // stop the interval timer
+    clearInterval(interval);
+
+    // turn off event handlers
+    $(document).off();
+  }
+  
+}
