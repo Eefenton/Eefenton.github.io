37dcb55caac0e74174b999358b6c5812b88f9771
Eefenton <ethanfenton9@gmail.com>
2021-03-15 23:11:12 +0000

    “message”

--- Pong/index.js
+++ Pong/index.js
@@ -12,11 +12,15 @@ function runProgram(){
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
 
   var KEY = {
-      "UP": 38,
-      "DOWN": 40,
-      "W": 87,
-      "S": 83
+      UP: 38,
+      DOWN: 40,
+      W: 87,
+      S: 83,
+      ENTER: 13,
   };
+
+  var boardWidth = $('#board').width();
+  var boardHeight = $('#board').height();
 
   // Game Item Objects
   function factory($id){
@@ -36,14 +40,15 @@ function runProgram(){
   var paddle2 = factory("#paddle2");
   var ball = factory("#ball");
   
+
   // assigning default speeds //
- ball.speedX = 5,
+ ball.speedX = 1,
 
   console.log(ball);
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('keyDown', handleKeyDown);     
-  $(document).on('keyUp', handleKeyUp);           
+  $(document).on('keydown', handleKeyDown);     
+  $(document).on('keyup', handleKeyUp);           
    // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -55,23 +60,39 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
  // makes it move but does not redraw
- function moveObject(gameObject){
-  
-moveObject(paddle1);
-moveObject(paddle2);
-moveObject(ball);
+ function moveObject(objects){
+  objects.speedY
+  objects.y
+ ball.x += ball.speedX
+ paddle1.y += paddle1.speedY
  }
  // redraws the object after being moved
- function updatePosition(gameObject){
-
-moveObject(paddle1);
-moveObject(paddle2);
-moveObject(ball);
+ function updatePosition(objects) {
+  $(ball.id).css("left", ball.x)
+  $(paddle1.id).css("top", paddle1.y)
  }
   
   function newFrame() {
-    //update ball position
+    //updateposition
+// paddle1 
+ moveObject(paddle1);
+ updatePosition(paddle1);
+ //paddle2
+ moveObject(paddle2);
+ updatePosition(paddle2);
+ //ball
+ moveObject(ball);
+ updatePosition(ball);
+    // handle collisions
     
+    // TEMPORARY handles ball/wall collision
+if (ball.x > boardWidth) {
+    ball.speedX = -ball.speedX;
+ }
+if (ball.x < 0) { 
+    ball.speedX = -ball.speedX; 
+ }
+   
   }
   
   /* 
@@ -80,25 +101,45 @@ moveObject(ball);
   function handleKeyDown(event) {
     if (event.which === KEY.S) {
            console.log("S");
-           speedY = 5;
+           paddle1.speedY = 5;
     }
     if (event.which === KEY.W) {
            console.log("W");
-           speedY = -5;
+           paddle1.speedY = -5;
+    }
+    if (event.which === KEY.DOWN) {
+           console.log("DOWN");
+           paddle2.speedY = 5;
+    }
+    if (event.which === KEY.UP) {
+           console.log("UP");
+           paddle2.speedY = -5;
     }
   }
    function handleKeyUp(event) {
       if (event.which === KEY.S) {
-          speedY = 0;
+          paddle1.speedY = 0;
       }
       if (event.which === KEY.W) {
-          speedY = 0
+          paddle1.speedY = 0
       }
     }
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
   
+  // collision functions
+  function doCollide(obj1, obj2) {
+      if (obj1.x = obj2.x) {
+          doCollide = true
+      }
+      if (doCollide = true) {
+          console.log ("collision!");
+      }
+      if (doCollide(paddle1, ball)) {
+          ball.speedX = -ball.speedX
+      }
+  }
   
   function endGame() {
     // stop the interval timer
