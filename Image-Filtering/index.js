1e6d0366b664dc8ed04b7d59282bdc8e6d006e58
Eefenton <ethanfenton9@gmail.com>
2021-05-10 22:44:13 +0000

    fixing .git issue

---
+++ Image-Filtering/index.js
@@ -0,0 +1,44 @@
+// This is a small program. There are only two sections. This first section is what runs
+// as soon as the page loads and is where you should call your functions.
+$(document).ready(function(){
+    const $display = $('#display');
+
+    // TODO: Call your apply function(s) here
+applyFilter(reddify);
+
+
+
+
+    render($display, image);
+});
+
+/////////////////////////////////////////////////////////
+// "apply" and "filter" functions should go below here //
+/////////////////////////////////////////////////////////
+
+// TODO 1 & 3: Create the applyFilter function here
+function applyFilter (filterFunction) {
+
+}
+
+for (var i = 0; i < image.length; i++) {
+    
+    for (var j = 0; j < image[i].length; j++) {
+        var rgbString = [i][j]
+        var rgbNumbers =  rgbStringToArray(rgbString);
+        filterFunction(rgbNumbers);
+        var rgbString = rgbArrayToString(rgbNumbers);
+        image[i, j] = rgbString
+    }
+    
+}
+
+// TODO 5: Create the applyFilterNoBackground function
+
+
+// TODO 2 & 4: Create filter functions
+function reddify (array){
+console.log(array[RED] = 255);
+}
+
+// CHALLENGE code goes below here
